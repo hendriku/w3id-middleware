0d01a7623daf414cd3fbb30a6435453718baaa6b
Hendrik Ulbrich <Hendrik.Ulbrich@de.ibm.com>
2020-11-02 22:13:00 +0100

    Lint tests

--- test/app.js
+++ test/app.js
@@ -1,35 +1,28 @@
+const port = process.env.PORT || "3000"
 
-const debug = require('debug')('test:server');
-const port = process.env.PORT || '3000';
-const http = require('http');
+const express = require("express")
 
-const express = require('express');
-const app = express();
+const app = express()
 
-const w3id = require(`${__dirname}/../index`);
+const w3id = require(`${__dirname}/../index`)
 
-app.set('port', port);
+app.set("port", port)
 
-app.get('/logout', (req, res, next) => {
+app.get("/logout", (req, res) => {
+	const oneWeekInMilliseconds = 604800000
 
-    const oneWeekInMilliseconds = 604800000;
-  
-    res.cookie( 'w3id_challenge', 1, { httpOnly : false, maxAge : oneWeekInMilliseconds } );
-    console.log('VARS:', res.cookies, process.env.NODE_ENV);
-    res.end();
-  
-});
-  
-app.get('/', (req, res, next) => {
-  
-    res.end();
-  
-});
-  
-app.use(w3id);
-  
-app.get('/protected',function(req, res, next) {
-    res.end();
-});
+	res.cookie("w3id_challenge", 1, { httpOnly: false, maxAge: oneWeekInMilliseconds })
+	res.end()
+})
 
-module.exports = app;
+app.get("/", (req, res) => {
+	res.end()
+})
+
+app.use(w3id)
+
+app.get("/protected", (req, res) => {
+	res.end()
+})
+
+module.exports = app
